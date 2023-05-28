30f7ade186c9b04be5171493ed645c6c1a464f36
Tsvetelina <tv49992041@edu1.pgzaimov.com>
2023-05-29 00:28:17 +0300

    designing

--- AnimalWorld/Controllers/HomeController.cs
+++ AnimalWorld/Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace AnimalWorld.Controllers
         {
             return View();
         }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
